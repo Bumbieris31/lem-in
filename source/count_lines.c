3c759718d200e04a5636208e06e56e5e56e20d3e
Flint Hignett <fhignett@f1r2s20.codam.nl>
2019-11-18 17:29:03 +0100

    calc lines added

--- source/count_lines.c
+++ source/count_lines.c
@@ -1,36 +1,18 @@
 #include "lem-in.h"
 
-static int	calc_longest_path(t_link *paths)
-{
-	t_link *path;
-	int		longest_path;
-
-	longest_path = 0;
-	path = paths;
-	while (path)
-	{
-		if (path->on > longest_path)
-			longest_path = path->on;
-		path = path->next;
-	}
-	return (longest_path);
-}
-
 void		count_lines(t_lemin *lemin)
 {
 	int		lines;
-	int		longest_path;
 	int		path_amount;
 	t_link	*path;
 
 	path = PATHS;
 	lines = lemin->ants;
-	longest_path = calc_longest_path(PATHS);
 	path_amount = 0;
 	while (path)
 	{
 		path_amount++;
-		lines += longest_path - path->on;
+		lines += path->on - 1;
 		path = path->next;
 	}
 	lines = lines / path_amount;
