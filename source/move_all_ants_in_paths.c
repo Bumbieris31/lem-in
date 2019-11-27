5a92c5eed3c524c2401a2c7e8c208e42e54ee787
Arturs Bumbieris <abumbier@f1r4s7.codam.nl>
2019-11-27 17:36:38 +0100

    move_all_ants normed

--- source/move_all_ants_in_paths.c
+++ source/move_all_ants_in_paths.c
@@ -6,7 +6,7 @@
 /*   By: abumbier <abumbier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 13:49:59 by abumbier          #+#    #+#             */
-/*   Updated: 2019/11/26 19:00:24 by abumbier         ###   ########.fr       */
+/*   Updated: 2019/11/27 17:35:44 by abumbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,23 @@ static void		incr_ants(t_room *path)
 	path->ant = -1;
 }
 
-int				move_one_position(t_lemin *lemin, int *ant, int *last_ant, int count)
+void			choose_path_flow(int *ant, int *last_ant, int next_ant, \
+int ant_count)
 {
-	int		next_ant;
-	t_link	*paths;
+	if (*ant == ant_count)
+	{
+		if (next_ant)
+			*last_ant = 1;
+	}
+	else if (next_ant)
+		(*ant)++;
+}
+
+int				move_one_position(t_lemin *lemin, int *ant, int *last_ant, \
+int count)
+{
+	int			next_ant;
+	t_link		*paths;
 
 	paths = lemin->paths;
 	while (paths)
@@ -53,34 +66,16 @@ int				move_one_position(t_lemin *lemin, int *ant, int *last_ant, int count)
 		if (!*last_ant && (paths && paths->on <= (lemin->lines - (count - 1))))
 		{
 			paths->ptr->ant = *ant;
-			// if (*ant > 96)
-				// ft_putstr("\nand another one!!!\n");
 			next_ant = 1;
 		}
-		// if (*ant > 96)
-			// ft_printf("\n ------last_ant: %d\n ------paths->on: %d\n ------lemin->lines: %d\n ------lemin->lines - count: %d\n", *last_ant, paths->on, lemin->lines, lemin->lines - count);
-		if (*ant == lemin->ants)
-		{
-			// if (*ant > 96)
-			// 	ft_putstr(" ant == lemin->ants ");
-			if (next_ant)
-				*last_ant = 1;
-		}
-		else if (next_ant)
-			(*ant)++;
+		choose_path_flow(ant, last_ant, next_ant, lemin->ants);
 		next_ant = 0;
-		int	temp = print_movement(paths->ptr, END);
-		if (temp == lemin->ants)
+		if (lemin->ants == print_movement(paths->ptr, END))
 			return (0);
 		paths = paths->next;
 	}
 	return (1);
 }
-
-/*
-** if one_pos has not added ant (in all the nodes in a loop) and last_ant == 0, then dont print "\n" and ont increment count. Instead increment var x = 0 in lemin->lines - (count - x). !
-*/
-
 
 /*
 ** @descr: Moves ants through the paths saved in lemin->paths
@@ -97,17 +92,13 @@ int				move_ants_in_all_paths(t_lemin *lemin)
 	count = 0;
 	last_ant = 0;
 	finished = 0;
-		int i = 0;
 	ft_printf("MIN LINE COUNT: %d\n", lemin->lines);
-	while (!finished)// && count != lemin->lines + 1
+	while (!finished)
 	{
 		if (!move_one_position(lemin, &ant, &last_ant, count))
 			finished = 1;
 		count++;
 		ft_putchar('\n');
-		i++;
-		// if (i > lemin->lines)
-			// break ;
 	}
 	ft_printf("OUR LINE COUNT: %d\n", count);
 	return (count);
